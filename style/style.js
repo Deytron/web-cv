9a4937969393c16fd4ec3cc7ba1c0a142c47a845
Deytron <romain.ranaivoson@ynov.com>
2022-03-03 21:42:26 +0100

    fixed navbar items position center

--- style/style.js
+++ style/style.js
@@ -1,27 +1,30 @@
 // NAVBAR BURGER
 document.addEventListener("DOMContentLoaded", () => {
-    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll(".navbar-burger"), 0);
-    if ($navbarBurgers.length > 0) {
-        $navbarBurgers.forEach((el) => {
-            el.addEventListener("click", () => {
-                const target = el.dataset.target;
-                const $target = document.getElementById(target);
-                el.classList.toggle("is-active");
-                $target.classList.toggle("is-active");
-            });
-        });
-    }
+  const $navbarBurgers = Array.prototype.slice.call(
+    document.querySelectorAll(".navbar-burger"),
+    0
+  );
+  if ($navbarBurgers.length > 0) {
+    $navbarBurgers.forEach((el) => {
+      el.addEventListener("click", () => {
+        const target = el.dataset.target;
+        const $target = document.getElementById(target);
+        el.classList.toggle("is-active");
+        $target.classList.toggle("is-active");
+      });
+    });
+  }
 });
 
 // RETURN TO TOP
-// $(document).ready(function () {
-//     // Retour haut de page
-//     $(this).scrollTop(0);
-// });
+$(document).ready(function () {
+  // Retour haut de page
+  $(this).scrollTop(0);
+});
 
 // ARROW CLICK AND GO TO SELECTION
 $("#arrowClick").click(function (e) {
-    $("html, body").animate({ scrollTop: $("#gotoBio").offset().top }, 10);
+  $("html, body").animate({ scrollTop: $("#gotoBio").offset().top }, 10);
 });
 
 // COLOR JAUNE CATEGORIES
@@ -41,56 +44,33 @@ projets.style.transition = "all 1s";
 
 // COLOR SWITCHING ON SCROLL
 window.addEventListener("scroll", () => {
-    if (window.scrollY < 750 && x.matches) {
-        spancolor.forEach((item) => {
-            item.style.color = "white";
-        });
-    }
-    if (window.scrollY >= 750 && x.matches) {
-        spancolor.forEach((item) => {
-            item.style.color = "black";
-        });
-    }
-    if (window.scrollY < 750 && !x.matches) {
-        competences.style.color = "white";
-        qualifs.style.color = "white";
-        projets.style.color = "white";
-        exp.style.color = "white";
-        bio.style.color = "white";
-    }
-    if (window.scrollY >= 750 && !x.matches) {
-        bio.style.color = "var(--yellow)";
-        competences.style.color = "black";
-        qualifs.style.color = "black";
-        exp.style.color = "black";
-        projets.style.color = "black";
-    }
-    if (window.scrollY >= 1600 && !x.matches) {
-        bio.style.color = "black";
-        competences.style.color = "var(--yellow)";
-        qualifs.style.color = "black";
-        exp.style.color = "black";
-        projets.style.color = "black";
-    }
-    if (window.scrollY >= 2500 && !x.matches) {
-        competences.style.color = "black";
-        exp.style.color = "black";
-        qualifs.style.color = "var(--yellow)";
-        projets.style.color = "black";
-        bio.style.color = "black";
-    }
-    if (window.scrollY >= 3000 && !x.matches) {
-        competences.style.color = "black";
-        exp.style.color = "var(--yellow)";
-        qualifs.style.color = "black";
-        projets.style.color = "black";
-        bio.style.color = "black";
-    }
-    if (window.scrollY >= 4000 && !x.matches) {
-        competences.style.color = "black";
-        exp.style.color = "var(--yellow)";
-        qualifs.style.color = "black";
-        projets.style.color = "black";
-        bio.style.color = "black";
-    }
+  if (window.scrollY < 750 && x.matches) {
+    // If at top
+    spancolor.forEach((item) => {
+      item.style.color = "white";
+    });
+  }
+  if (window.scrollY >= 750 && x.matches) {
+    // If at bio
+    spancolor.forEach((item) => {
+      // All texts go black
+      item.style.color = "black";
+    });
+  }
+  if (window.scrollY < 750 && !x.matches) {
+    competences.style.color = "white";
+    qualifs.style.color = "white";
+    projets.style.color = "white";
+    exp.style.color = "white";
+    bio.style.color = "white";
+    // Navbar gets transparent
+    document.getElementById("navbar").classList.add("is-transparent");
+    document.getElementById("navbar").classList.remove("is-white");
+    document.getElementById("navbar").classList.remove("has-shadow");
+  }
+  if (window.scrollY >= 750 && !x.matches) {
+    document.getElementById("navbar").classList.remove("is-transparent");
+    document.getElementById("navbar").classList.add("is-white");
+    document.getElementById("navbar").classList.add("has-shadow");
+  }
 });
